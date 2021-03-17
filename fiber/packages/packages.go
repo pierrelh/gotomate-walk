2279ff4323efe570c633702d4af40fe0e5a7b8ab
Pierre L'Hopital <lhopital.pierre@gmail.com>
2021-03-17 12:39:52 +0100

    Rework primary & secondary listboxes models creation

--- fiber/packages/packages.go
+++ fiber/packages/packages.go
@@ -10,34 +10,40 @@ import (
 	"strings"
 )
 
+// Package Declare the structure of a package
+type Package struct {
+	Name      string
+	Functions []string
+}
+
 // Packages Export all the available packages & their functions
 var Packages = getAutomatePackages()
 
 // getAutomatePackages Get & return all the available packages & their functions
-func getAutomatePackages() map[string][]string {
+func getAutomatePackages() []*Package {
 	const subPackage = "fiber/packages"
-	packs := make(map[string][]string)
+	var packs []*Package
 
 	filepath.Walk(subPackage, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == "" && filepath.Base(path) != "packages" {
-			packs[filepath.Base(path)] = []string{}
-		} else {
-			if filepath.Base(path) == "functions.go" {
-				set := token.NewFileSet()
-				pack, err := parser.ParseFile(set, path, nil, 0)
-				if err != nil {
-					fmt.Println("GOTOMATE FATAL ERROR: Unable to parse the file", err)
-					os.Exit(1)
-				}
+		if filepath.Base(path) == "functions.go" {
+			pathSlice := strings.Split(filepath.Dir(path), "\\")
+			directory := pathSlice[len(pathSlice)-1]
+			newPackage := &Package{
+				Name: directory,
+			}
+			set := token.NewFileSet()
+			pack, err := parser.ParseFile(set, path, nil, 0)
+			if err != nil {
+				fmt.Println("GOTOMATE FATAL ERROR: Unable to parse the file", err)
+				os.Exit(1)
+			}
 
-				for _, d := range pack.Decls {
-					if fn, isFn := d.(*ast.FuncDecl); isFn {
-						pathSlice := strings.Split(filepath.Dir(path), "\\")
-						directory := pathSlice[len(pathSlice)-1]
-						packs[directory] = append(packs[directory], fn.Name.String())
-					}
+			for _, d := range pack.Decls {
+				if fn, isFn := d.(*ast.FuncDecl); isFn {
+					newPackage.Functions = append(newPackage.Functions, fn.Name.String())
 				}
 			}
+			packs = append(packs, newPackage)
 		}
 		return nil
 	})
